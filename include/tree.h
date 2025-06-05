33a0e37104512f99f7e0a96044bc42f7a4e5738e
ossicc <ossic8881@gmail.com>
2025-06-06 00:04:56 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,4 +1,5 @@
 // Copyright 2022 NNTU-CS
+#pragma once
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
@@ -10,27 +11,27 @@ class PMTree {
  public:
   struct Node {
     char value;
-    std::vector<std::shared_ptr<Node>> children;
+    std ::vector<std ::shared_ptr<Node>> children;
     explicit Node(char val) : value(val) {}
   };
 
-  explicit PMTree(const std::vector<char>& elements);
+  explicit PMTree(const std ::vector<char>& elements);
   ~PMTree() = default;
 
-  void create_tree(std::shared_ptr<Node>& parent,
-                   const std::vector<char>& remain);
-  void create(const std::vector<char>& elements);
+  void create_tree(std ::shared_ptr<Node>& parent,
+                   const std ::vector<char>& remain);
+  void create(const std ::vector<char>& elements);
 
-  std::shared_ptr<Node> getRoot() const { return root; }
+  std ::shared_ptr<Node> getRoot() const { return root; }
   int getTotalPermutations() const { return total_permutations; }
 
  private:
-  std::shared_ptr<Node> root;
+  std ::shared_ptr<Node> root;
   int total_permutations;
 };
 
-std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
-std::vector<char> getPerm1(const PMTree& tree, int num);
-std::vector<char> getPerm2(const PMTree& tree, int num);
+std ::vector<std ::vector<char>> getAllPerms(const PMTree& tree);
+std ::vector<char> getPerm1(const PMTree& tree, int num);
+std ::vector<char> getPerm2(const PMTree& tree, int num);
 
 #endif  // INCLUDE_TREE_H_
