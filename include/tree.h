7a4c50b7b6f06b74c605de1b1dceb44be2f66102
ossicc <ossic8881@gmail.com>
2025-06-05 23:51:36 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -13,17 +14,12 @@ class PMTree {
     explicit Node(char val) : value(val) {}
   };
 
-  // Constructor builds the tree immediately using create_tree
-  explicit PMTree(const std::vector<char>& elements)
-      : root(nullptr), total_permutations(0) {
-    create_tree(root, elements);
-  }
-
+  explicit PMTree(const std::vector<char>& elements);
   ~PMTree() = default;
 
   void create_tree(std::shared_ptr<Node>& parent,
                    const std::vector<char>& remain);
-  void create(std::vector<char>& elements);
+  void create(const std::vector<char>& elements);
 
   std::shared_ptr<Node> getRoot() const { return root; }
   int getTotalPermutations() const { return total_permutations; }
@@ -36,6 +32,5 @@ class PMTree {
 std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
 std::vector<char> getPerm1(const PMTree& tree, int num);
 std::vector<char> getPerm2(const PMTree& tree, int num);
-std::vector<char> generateAlphabet(int n);
 
 #endif  // INCLUDE_TREE_H_
